6eee475e54a7ff8a1c2f9f52b3f6290b7377b7c8
nizarhalloun <nshalloun@hotmail.com>
2020-11-12 12:45:56 +0200

    clean code

--- public/index.js
+++ public/index.js
@@ -1,24 +1,58 @@
-document.getElementById('submit').addEventListener('click', () => {
-  const input = document.getElementById('input');
-  const value = input.value.trim();
+const input = document.getElementById('input');
+const result = document.querySelector('.result');
+const spinner = document.getElementById('spinner');
 
-  const result = document.querySelector('.result');
+function showErrorAlert(message) {
+  const alert = document.querySelector('.alert-danger');
+  alert.innerHTML = '';
+  alert.innerHTML = message;
+  alert.classList.remove('hide');
+}
+
+function removeErrorAlert() {
+  const alert = document.querySelector('.alert-danger');
+  alert.classList.add('hide');
+}
+
+function showTranslatedResult(morseCode) {
+  const newHeader = document.createElement('h1');
+  newHeader.textContent = morseCode;
+  result.appendChild(newHeader);
+  result.classList.remove('hide');
+}
+
+function removeTranslatedResult() {
   result.innerHTML = '';
-  if (!value) return;
-  input.value = '';
-  const spinner = document.getElementById('spinner');
-  spinner.classList.remove('hide');
+  result.classList.add('hide');
+}
 
+function showSpinner() {
+  spinner.classList.remove('hide');
+}
+
+function hideSpinner() {
+  spinner.classList.add('hide');
+}
+
+document.getElementById('submit').addEventListener('click', () => {
+  const value = input.value.trim();
+  removeErrorAlert();
+  removeTranslatedResult();
+  if (!value) {
+    showErrorAlert('Please enter a text before submitting');
+    return;
+  }
+  input.value = '';
+  showSpinner();
   fetch(`/data?data=${value}`)
     .then((body) => body.json())
     .then((json) => {
-      const newHeader = document.createElement('h1');
-      newHeader.textContent = json.morsecode;
-      result.appendChild(newHeader);
-      result.classList.remove('hide');
-      spinner.classList.add('hide');
+      showTranslatedResult(json.morsecode);
+      hideSpinner();
     })
     .catch((err) => {
+      hideSpinner();
+      showErrorAlert('Server Error');
       throw err;
     });
 });
